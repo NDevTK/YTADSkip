e28f011435a0e6d4c169f77afbb06e590d6bbd6e
NDevTK <31563761+NDevTK@users.noreply.github.com>
2019-01-15 20:40:58 +0000

    Run with Beautifier
    https://beautifier.io/

--- background.js
+++ background.js
@@ -1,14 +1,15 @@
 chrome.tabs.onUpdated.addListener(
-  function(tabId, changeInfo, tab) {
-    if (changeInfo.url) {
-	  if(youtube_parser(changeInfo.url)){
-      chrome.tabs.sendMessage( tabId, {
-        message: 'ytvideo'
-      })
-	  }
+    function(tabId, changeInfo, tab) {
+        if (changeInfo.url) {
+            if (youtube_parser(changeInfo.url)) {
+                chrome.tabs.sendMessage(tabId, {
+                    message: 'ytvideo'
+                })
+            }
+        }
     }
-  }
 );
+
 function youtube_parser(url) {
     var regExp = /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#\&\?]*).*/;
     var match = url.match(regExp);
