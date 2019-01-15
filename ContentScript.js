1fbbddd5c3830d4c50ddc75429161318ca384dfc
NDevTK <31563761+NDevTK@users.noreply.github.com>
2019-01-15 20:41:37 +0000

    Run with beautifier
    https://beautifier.io/

--- ContentScript.js
+++ ContentScript.js
@@ -1,13 +1,18 @@
 chrome.runtime.onMessage.addListener(
-  function(request, sender, sendResponse) {
-    if (request.message === 'ytvideo') {
-	setTimeout(function(){ SkipAdvert() }, 2000);
-    }
-});
+    function(request, sender, sendResponse) {
+        if (request.message === 'ytvideo') {
+            setTimeout(function() {
+                SkipAdvert()
+            }, 2000);
+        }
+    });
 
-function SkipAdvert(){
-if(document.getElementsByClassName("ytp-ad-player-overlay").length == 1){
-var v = document.querySelector('video');
-v.currentTime = 1000
-setTimeout(function(){ v.play() }, 100);
-}}
+function SkipAdvert() {
+    if (document.getElementsByClassName("ytp-ad-player-overlay").length == 1) {
+        var v = document.querySelector('video');
+        v.currentTime = 1000
+        setTimeout(function() {
+            v.play()
+        }, 100);
+    }
+}
