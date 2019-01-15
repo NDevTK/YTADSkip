22909a5cf1b010eb5c1120457b95ddbc68cbda37
NDevTK <31563761+NDevTK@users.noreply.github.com>
2019-01-15 18:30:14 +0000

    Update ContentScript.js

--- ContentScript.js
+++ ContentScript.js
@@ -1,13 +1,12 @@
-var v = document.querySelector('video')
-var v_id = youtube_parser(document.URL);
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    if (request.message === 'ytvideo') {
+      SkipAdvert(request.url);
+    }
+});
 
-if (v_id) {
+function SkipAdvert(){
+var v = document.querySelector('video');
 v.currentTime = 1000;
 setTimeout(function(){ v.play() }, 100);
-}
-
-function youtube_parser(url) {
-    var regExp = /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#\&\?]*).*/;
-    var match = url.match(regExp);
-    return (match && match[7].length == 11) ? match[7] : false;
 }
