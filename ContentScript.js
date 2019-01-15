88eec24f2dc98b9cafcaa0b2e3e7982f7a13ffe2
NDevTK <31563761+NDevTK@users.noreply.github.com>
2019-01-15 14:28:08 +0000

    Update ContentScript.js

--- ContentScript.js
+++ ContentScript.js
@@ -1,9 +1,13 @@
+var v = document.querySelector('video')
+var v_id = youtube_parser(document.URL);
+
+if (v_id) {
+v.currentTime = 1000;
+setTimeout(function(){ v.play() }, 100);
+}
+
 function youtube_parser(url) {
     var regExp = /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#\&\?]*).*/;
     var match = url.match(regExp);
     return (match && match[7].length == 11) ? match[7] : false;
-}
-if (youtube_parser(document.URL)) {
-document.querySelector('video').currentTime = 1000;
-setTimeout(function(){ document.querySelector('video').play() }, 100);
 }
