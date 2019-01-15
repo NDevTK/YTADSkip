615b5e7e41b1f95ea7427b28981bd86a1ff8fc70
NDevTK <31563761+NDevTK@users.noreply.github.com>
2019-01-15 20:07:27 +0000

    Update ContentScript.js

--- ContentScript.js
+++ ContentScript.js
@@ -1,12 +1,13 @@
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
     if (request.message === 'ytvideo') {
-      SkipAdvert(request.url);
+	setTimeout(function(){ SkipAdvert() }, 1400);
     }
 });
 
 function SkipAdvert(){
+if(document.getElementsByClassName("ytp-ad-player-overlay").length == 1){
 var v = document.querySelector('video');
-v.currentTime = 1000;
+v.currentTime = 1000
 setTimeout(function(){ v.play() }, 100);
-}
+}}
